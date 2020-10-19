7aac342cea9cce2cd3ccae34263ee171487a7cc3
Đặng Hùng <danghungtb26@gmail.com>
2020-10-19 17:37:20 +0700

    remove: old schema, change: method in user_schema

--- Server/api/database/Schemas/User.ts
+++ Server/api/database/Schemas/User.ts
@@ -40,6 +40,47 @@ const method = {
   getPhone: function getPhone(): string {
     return this.phone
   },
+  /**
+   *
+   * @param password
+   * @param callback
+   * func compare xem user đã đăng nhập đúng hay chưa
+   * có 2 cách trả về là callback: (error và success)
+   */
+  compare: function compare(password: string, callback: (error, value) => void): Promise<any> {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (typeof callback === 'function') callback(err, isMatch)
+        if (err) {
+          reject(err)
+        }
+        resolve(isMatch)
+      })
+    })
+  },
+  /**
+   *
+   * @param callback
+   * func này sử dụng salt để generate ra hash_password lưu vào trong database
+   */
+  generate: function generate(callback: (error, success) => void): Promise<any> {
+    return new Promise((resolve, reject) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          if (typeof callback === 'function') callback(err, null)
+          reject(err)
+        }
+        bcrypt.hash(this.password, salt, (err2, hash) => {
+          if (typeof callback === 'function') callback(err2, hash)
+          if (err2) {
+            reject(err2)
+          }
+          this.password = hash
+          resolve(hash)
+        })
+      })
+    })
+  },
 }
 
 const User = new Schema<typeof method>(
