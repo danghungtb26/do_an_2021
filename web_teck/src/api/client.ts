c578122096e5beb66fa72f7b406ef046b7856d4e
Đặng Hùng <danghungtb26@gmail.com>
2020-12-08 15:47:21 +0700

    base url

--- web_teck/src/api/client.ts
+++ web_teck/src/api/client.ts
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client'
 
-// export const baseUrlImage = 'https://do-an-2021.herokuapp.com/image/'
-// const base_url = 'https://do-an-2021.herokuapp.com/graphql'
-const base_url = 'http://localhost:6161/graphql'
-export const baseUrlImage = 'http://localhost:6161/image/'
+export const baseUrlImage = 'https://do-an-2021.herokuapp.com/image/'
+const base_url = 'https://do-an-2021.herokuapp.com/graphql'
+// const base_url = 'http://localhost:6161/graphql'
+// export const baseUrlImage = 'http://localhost:6161/image/'
 
 const client = new ApolloClient({
   // uri: base_url,
